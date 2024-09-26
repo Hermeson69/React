8fe8a560b1dbff6881f0e93bc86397b2d7809c79
Hermeson Alves <alveshermeson84@gmail.com>
2024-09-26 20:26:18 -0300

    React_up

--- teste2/src/App.jsx
+++ teste2/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* Outros componentes */}
+      <Slider />
     </div>
   );
 }
--- teste2/src/components/Navbar/index.jsx
+++ teste2/src/components/Navbar/index.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+// import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons'; 
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  // const toggleMenu = () => {
+  //   setIsMenuOpen(!isMenuOpen);
+  // };
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-header">
-          <a className="navbar-brand" href="/">BrandName</a>
+          <img className="navbar-logo" src="../../../public/Design_sem_nome__2_-removebg-preview.png" alt="" />
+          <a className="navbar-brand" href="/">Fang</a>
         </div>
         <ul className={`navbar-nav ${isMenuOpen ? 'open' : ''}`}>
           <li className="nav-item">
@@ -31,7 +32,20 @@ function Navbar() {
             <a className="nav-link" href="/contact">Dominios</a>
           </li>
         </ul>
-        <div className="navbar-icons">
+
+        <div className="navbar-user">
+          <li className="nav-item">
+            <a className="nav-link" href="/contact">Procura</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/contact">Conta</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/contact">Bag(2)</a>
+          </li>
+        </div>
+
+        {/* <div className="navbar-icons">
           <a className="nav-icon" href="https://github.com" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} />
           </a>
@@ -41,11 +55,10 @@ function Navbar() {
           <a className="nav-icon" href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-          <button>Contato</button>
-        </div>
-        <button className="menu-toggle" onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} />
-          </button>
+        </div> */}
+        {/* <button className="menu-toggle" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </button> */}
       </div>
     </nav>
   );
