187d0ad85461263550afbb4b2f2e78a057d24aef
Hermeson Alves <alveshermeson84@gmail.com>
2024-09-27 12:54:39 -0300

    Atualizacao

--- teste2/src/App.jsx
+++ teste2/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Slider from './components/Slider';
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Slider />
     </div>
   );
 }
